0eb1e7f59f65719e1164f2aac1a072e149641383
Waldo-Noe-Liberato-Jara <117687144+Waldo-Noe-Liberato-Jara@users.noreply.github.com>
2023-02-08 10:27:56 -0500

    Quinto commit

--- src/App.js
+++ src/App.js
@@ -1,41 +1,41 @@
 import './App.css';
 import Testimonio from './componentes/Testimonio.js';
 
+const Testimonios = [
+  {nombre:'Rick Grimes', ciudad:'Alexandría', cargo:'Líder', empresa:'Alexandría', imagen:'rick-grimes.jpg', testimonio:"No lamento todo lo que dije <strong>ayer</strong>, sólo siento no haberlo dicho antes."},
+  {nombre:'Shane', ciudad:'Georgia', cargo:'Sheriff', empresa:'Condado de King', imagen:'shane.jpg', testimonio:"Sobrevivir, amigo. Significa tomar decisiones difíciles."},
+  {nombre:'Gobernador', ciudad:'Georgia', cargo:'Líder', empresa:'Woodbury', imagen:'gobernador.jpg', testimonio:'Te unes a mí. Te prometo que nunca tendrás que preocuparte por hacer lo correcto o lo incorrecto.'},
+  {nombre:'Negan', ciudad:'Washington, D.C.', cargo:'Líder', empresa:'Los Salvadores', imagen:'negan.jpg', testimonio:'El castigo es algo imprescindible. La penitencia, esa es la base de todo lo que tenemos.'},
+];
+
+var textito = "<strong>Importante</strong>";
+/*
+Testimonios.forEach(function(elemento, indice, array) {
+  console.log(elemento.nombre);
+})*/
+
 function App() {
   return (
     <div className='App'>
-      <h1 className='Titulo'>Personajes de The Walking Dead</h1>
-      <Testimonio
-      nombre='Rick Grimes'
-      ciudad='Alexandría'
-      cargo='Líder'
-      empresa='Alexandría'
-      imagen='rick-grimes.jpg'
-      testimonio='No lamento todo lo que dije ayer, sólo siento no haberlo dicho antes.' />
+      <h1 className='Titulo' dangerouslySetInnerHTML={{ __html: "hola"+textito }}></h1>
 
-      <Testimonio
-      nombre='Shane'
-      ciudad='Georgia'
-      cargo='Sheriff'
-      empresa='Condado de King'
-      imagen='shane.jpg'
-      testimonio='Sobrevivir, amigo. Significa tomar decisiones difíciles.' />
+      {
+      Testimonios.map((elemento, index) => {
+          return (
+            <Testimonio
+              key={index}
+              nombre={elemento.nombre}
+              ciudad={elemento.ciudad}
+              cargo={elemento.cargo}
+              empresa={elemento.empresa}
+              imagen={elemento.imagen}
+              testimonio={elemento.testimonio}
+            />
+          );
+        }
+                    )
+        }
 
-      <Testimonio
-      nombre='Gobernador'
-      ciudad='Georgia'
-      cargo='Líder'
-      empresa='Woodbury'
-      imagen='gobernador.jpg'
-      testimonio='Te unes a mí. Te prometo que nunca tendrás que preocuparte por hacer lo correcto o lo incorrecto.' />
-        
-      <Testimonio 
-      nombre='Negan'
-      ciudad='Washington, D.C.'
-      cargo='Líder'
-      empresa='Los Salvadores'
-      imagen='negan.jpg'
-      testimonio='El castigo es algo imprescindible. La penitencia, esa es la base de todo lo que tenemos.' />
     </div>
   );
 }
--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -7,7 +7,7 @@ function Testimonio(props) {
       <div className='contenedor-texto-testimonio'>
         <p className='nombre-testimonio'>{props.nombre} en {props.ciudad}</p>
         <p className='cargo-testimonio'>{props.cargo} de {props.empresa}</p>
-        <p className='texto-testimonio'>{props.testimonio}</p>
+        <p className='texto-testimonio' dangerouslySetInnerHTML={{ __html: props.testimonio }} ></p>
       </div>
     </div>
   );
