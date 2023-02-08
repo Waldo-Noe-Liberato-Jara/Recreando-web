9a549179ab9b6346eab0ada0ea534b5712db5f6f
Waldo-Noe-Liberato-Jara <117687144+Waldo-Noe-Liberato-Jara@users.noreply.github.com>
2023-02-08 11:02:59 -0500

    Sexto commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,30 @@ import './App.css';
 import Testimonio from './componentes/Testimonio.js';
 
 const Testimonios = [
-  {nombre:'Rick Grimes', ciudad:'Alexandría', cargo:'Líder', empresa:'Alexandría', imagen:'rick-grimes.jpg', testimonio:"No lamento todo lo que dije <strong>ayer</strong>, sólo siento no haberlo dicho antes."},
-  {nombre:'Shane', ciudad:'Georgia', cargo:'Sheriff', empresa:'Condado de King', imagen:'shane.jpg', testimonio:"Sobrevivir, amigo. Significa tomar decisiones difíciles."},
-  {nombre:'Gobernador', ciudad:'Georgia', cargo:'Líder', empresa:'Woodbury', imagen:'gobernador.jpg', testimonio:'Te unes a mí. Te prometo que nunca tendrás que preocuparte por hacer lo correcto o lo incorrecto.'},
-  {nombre:'Negan', ciudad:'Washington, D.C.', cargo:'Líder', empresa:'Los Salvadores', imagen:'negan.jpg', testimonio:'El castigo es algo imprescindible. La penitencia, esa es la base de todo lo que tenemos.'},
+  { nombre: 'Rick Grimes', ciudad: 'Alexandría', cargo: 'Líder', empresa: 'Alexandría', imagen: 'rick-grimes.jpg', testimonio: "No lamento todo lo que dije <strong>ayer</strong>, sólo siento no haberlo dicho antes." },
+  { nombre: 'Shane', ciudad: 'Georgia', cargo: 'Sheriff', empresa: 'Condado de King', imagen: 'shane.jpg', testimonio: "Sobrevivir, amigo. Significa tomar decisiones difíciles." },
+  { nombre: 'Gobernador', ciudad: 'Georgia', cargo: 'Líder', empresa: 'Woodbury', imagen: 'gobernador.jpg', testimonio: 'Te unes a mí. Te prometo que nunca tendrás que preocuparte por hacer lo correcto o lo incorrecto.' },
+  { nombre: 'Negan', ciudad: 'Washington, D.C.', cargo: 'Líder', empresa: 'Los Salvadores', imagen: 'negan.jpg', testimonio: 'El castigo es algo imprescindible. La penitencia, esa es la base de todo lo que tenemos.' },
 ];
 
-var textito = "<strong>Importante</strong>";
-/*
-Testimonios.forEach(function(elemento, indice, array) {
-  console.log(elemento.nombre);
-})*/
+var cantidad = 5;
+var order = 1;
+        
+for(var i = 1; i <= Testimonios.length; i++){
+    console.log(order);
+    order = order + 1;
+    if(order > 2){
+        order = 1;
+    }
+}
 
 function App() {
   return (
     <div className='App'>
-      <h1 className='Titulo' dangerouslySetInnerHTML={{ __html: "hola"+textito }}></h1>
+      <h1 className='Titulo'>Personajes de The Walking Dead</h1>
 
       {
-      Testimonios.map((elemento, index) => {
+        Testimonios.map((elemento, index) => {
           return (
             <Testimonio
               key={index}
@@ -33,8 +38,8 @@ function App() {
             />
           );
         }
-                    )
-        }
+        )
+      }
 
     </div>
   );
--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Testimonio(props) {
   return(
     <div className='contenedor-testimonio'>
-      <img className='imagen-testimonio' src={ require(`../imagenes/${props.imagen}`) } alt={props.nombre} />
+      <img style={props.estilo_imagen} className='imagen-testimonio' src={ require(`../imagenes/${props.imagen}`) } alt={props.nombre} />
       <div className='contenedor-texto-testimonio'>
         <p className='nombre-testimonio'>{props.nombre} en {props.ciudad}</p>
         <p className='cargo-testimonio'>{props.cargo} de {props.empresa}</p>
