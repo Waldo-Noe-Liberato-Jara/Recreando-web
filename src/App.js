8d9d753712c5b289bf19b6ec453d239c9ddd86ed
Waldo-Noe-Liberato-Jara <117687144+Waldo-Noe-Liberato-Jara@users.noreply.github.com>
2023-02-12 01:10:26 -0500

    12-02-2023 1:10 am

--- src/App.js
+++ src/App.js
@@ -1,34 +1,33 @@
+import { useState } from 'react';
 import './App.css';
 import Testimonio from './componentes/Testimonio.js';
 
 const Testimonios = [
   { nombre: 'Rick Grimes', ciudad: 'Alexandría', cargo: 'Líder', empresa: 'Alexandría', imagen: 'rick-grimes.jpg', testimonio: "No lamento todo lo que dije <strong>ayer</strong>, sólo siento no haberlo dicho antes." },
   { nombre: 'Shane', ciudad: 'Georgia', cargo: 'Sheriff', empresa: 'Condado de King', imagen: 'shane.jpg', testimonio: "Sobrevivir, amigo. Significa tomar decisiones difíciles." },
-  { nombre: 'Gobernador', ciudad: 'Georgia', cargo: 'Líder', empresa: 'Woodbury', imagen: 'gobernador.jpg', testimonio: 'Te unes a mí. Te prometo que nunca tendrás que preocuparte por hacer lo correcto o lo incorrecto.' },
+  { nombre: 'Gobernador', ciudad: 'Georgia', cargo: 'Líder', empresa: 'Woodbury', imagen: 'gobernador.jpg', testimonio: 'Si te unes a mí. Te prometo que nunca tendrás que preocuparte por hacer lo correcto o lo incorrecto.' },
   { nombre: 'Negan', ciudad: 'Washington, D.C.', cargo: 'Líder', empresa: 'Los Salvadores', imagen: 'negan.jpg', testimonio: 'El castigo es algo imprescindible. La penitencia, esa es la base de todo lo que tenemos.' },
 ];
 
-var cantidad = 5;
-var order = 1;
-        
-for(var i = 1; i <= Testimonios.length; i++){
-    console.log(order);
-    order = order + 1;
-    if(order > 2){
-        order = 1;
-    }
-}
-
 function App() {
+  const [testimonio, setTestimonio] = useState(Testimonios);
+  const AgregarTestimonio = () => {
+    setTestimonio([...testimonio, { nombre: 'Rick Grimes', ciudad: 'Alexandría', cargo: 'Líder', empresa: 'Alexandría', imagen: 'rick-grimes_02.jpg', testimonio: "No lamento todo lo que dije <strong>ayer</strong>, sólo siento no haberlo dicho antes." }]);
+  }
+
   return (
     <div className='App'>
       <h1 className='Titulo'>Personajes de The Walking Dead</h1>
-
+      <button onClick={AgregarTestimonio}>Agregar Personaje</button>
+      <script>console.log("a");</script>
       {
-        Testimonios.map((elemento, index) => {
+        testimonio.map((elemento, index) => {
           return (
             <Testimonio
               key={index}
+              estilo_btn_remover={ (index % 2 === 0) ? "end" : "start" }
+              estilo_imagen={ (index % 2 === 0) ? 1 : 2 }
+              estilo_texto={ (index % 2 === 0) ? 2 : 1 }
               nombre={elemento.nombre}
               ciudad={elemento.ciudad}
               cargo={elemento.cargo}
--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
+var contador = 0;
+function Clickear() {
+  contador = contador+1;
+  console.log(contador);
+}
+
 function Testimonio(props) {
-  return(
+  return (
     <div className='contenedor-testimonio'>
-      <img style={props.estilo_imagen} className='imagen-testimonio' src={ require(`../imagenes/${props.imagen}`) } alt={props.nombre} />
-      <div className='contenedor-texto-testimonio'>
+      <div className='remover-personaje' style={{ textAlign: props.estilo_btn_remover }}>
+        <span className='btn-remover' onClick={() => Clickear()}>x</span>
+      </div>
+
+      <img style={{ order: props.estilo_imagen }} className='imagen-testimonio' src={require(`../imagenes/${props.imagen}`)} alt={props.nombre} />
+      <div style={{ order: props.estilo_texto }} className='contenedor-texto-testimonio'>
         <p className='nombre-testimonio'>{props.nombre} en {props.ciudad}</p>
         <p className='cargo-testimonio'>{props.cargo} de {props.empresa}</p>
         <p className='texto-testimonio' dangerouslySetInnerHTML={{ __html: props.testimonio }} ></p>
